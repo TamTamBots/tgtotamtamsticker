ed5860451b56b311524e23e76e91bf6f783bfd52
ÁÑÑÍHÌLÅTØR SPÄRK <75305464+annihilatorrrr@users.noreply.github.com>
2022-09-10 17:40:41 +0530

    Update! (#1)
    Co-authored-by: deepsource-autofix[bot] <62050782+deepsource-autofix[bot]@users.noreply.github.com>

--- src/tg_to_tt_stickers/tg_sticker_downloader.py
+++ src/tg_to_tt_stickers/tg_sticker_downloader.py
@@ -14,8 +14,10 @@ from .image_converter import ImageConverter
 
 TELEGRAM_API_BASE_URL = "https://api.telegram.org"
 
+
 class TGStickerDownloaderException(Exception):
     pass
+
 
 @dataclass
 class Sticker:
@@ -23,18 +25,21 @@ class Sticker:
     file_bytes: bytes
     emoji: str
 
+
 @dataclass
 class StickersSet:
     name: str
     title: str
     stickers: List[Sticker]
 
+
 @dataclass()
 class TGFile:
     file_id: str
-    file_unique_id: str 
+    file_unique_id: str
     file_size: int
-    file_path: str # stickers/file_81.webp
+    file_path: str  # stickers/file_81.webp
+
 
 class TGStickerDownloader:
 
@@ -52,25 +57,25 @@ class TGStickerDownloader:
         except Exception:
             return None
         self.log.debug("resp: %s", json.dumps(res))
-        stickers = []
-        for sticker in res['result']['stickers']:
-            stickers.append(Sticker(
-                file_id=sticker['file_id'],
-                emoji=sticker['emoji'],
-                file_bytes=b""
-            ))
-        return StickersSet(res['result']['name'], res['result']['title'], stickers)
+        stickers = [
+            Sticker(file_id=sticker["file_id"],
+                    emoji=sticker["emoji"],
+                    file_bytes=b"") for sticker in res["result"]["stickers"]
+        ]
+        return StickersSet(res["result"]["name"], res["result"]["title"],
+                           stickers)
 
     def get_sticker_file(self, file_id) -> TGFile:
         method = "getFile"
-        res = self.api_request(method, {"file_id": file_id})['result']
-        return TGFile(res['file_id'], res['file_unique_id'], res['file_size'], res['file_path'])
+        res = self.api_request(method, {"file_id": file_id})["result"]
+        return TGFile(res["file_id"], res["file_unique_id"], res["file_size"],
+                      res["file_path"])
 
     def download_file(self, file_path) -> bytes:
         res = requests.get(f"{self.base_file_url}/{file_path}")
         return res.content
 
-    def api_request(self, method : str, params : dict=None) -> dict:
+    def api_request(self, method: str, params: dict = None) -> dict:
         url = f"{self.base_api_url}/{method}"
         self.log.debug("url: %s, params: %s", url, params)
         resp = requests.Response()  # type: requests.Response
@@ -81,7 +86,9 @@ class TGStickerDownloader:
         if resp.status_code == 200:
             return resp.json()
         else:
-            raise TGStickerDownloaderException(f"telegram API error, status code: {resp.status_code}, text: {resp.text}")
+            raise TGStickerDownloaderException(
+                f"telegram API error, status code: {resp.status_code}, text: {resp.text}"
+            )
 
     @classmethod
     def chunks(cls, lst, number_of_chunks):
@@ -89,40 +96,44 @@ class TGStickerDownloader:
         for i in range(0, len(lst), number_of_chunks):
             yield lst[i:i + number_of_chunks]
 
-    def create_tamtam_zip(self, tg_set: StickersSet) -> 'List[str]':
+    def create_tamtam_zip(self, tg_set: StickersSet) -> "List[str]":
         """return path to zip archive with stickers in TamTam format"""
         pool = Pool(10)
         result_files = []  # type: List[str]
         with TemporaryDirectory(suffix=tg_set.name) as tmpdir:
-            pool.starmap(self.proceed_sticker, [(x, tmpdir, tg_set.name, result_files) for x in tg_set.stickers])
+            pool.starmap(
+                self.proceed_sticker,
+                [(x, tmpdir, tg_set.name, result_files)
+                 for x in tg_set.stickers],
+            )
             result_zip_names = []
 
             parts = self.chunks(result_files, 50)
-            part_n = 0
-            for part in parts:
+            for part_n, part in enumerate(parts):
                 zip_name = f"{tg_set.name}_{part_n}.zip"
-                part_n += 1
                 with zipfile.ZipFile(zip_name, "w") as zf:
                     for sticker_file in part:
                         self.log.debug("add to archive: %s", sticker_file)
-                        zf.write(sticker_file, arcname=sticker_file.split("/")[-1])
+                        zf.write(sticker_file,
+                                 arcname=sticker_file.split("/")[-1])
                     result_zip_names.append(zip_name)
         return result_zip_names
 
-
-    def proceed_sticker(self, sticker: Sticker, tmp_dir : str, pack_name : str, result: list):
+    def proceed_sticker(self, sticker: Sticker, tmp_dir: str, pack_name: str,
+                        result: list):
         img_convertor = ImageConverter()
         self.log.debug("proceed sticker: %s", sticker)
         st_file = self.get_sticker_file(sticker.file_id)
         self.log.debug("proceed st_file %s", st_file)
         sticker.file_bytes = self.download_file(st_file.file_path)
 
-        rnd_postfix = ''.join(random.choice(ascii_letters) for _ in range(5))
+        rnd_postfix = "".join(random.choice(ascii_letters) for _ in range(5))
         if st_file.file_path.endswith("tgs"):
             self.log.debug("it's a tgs file, do not convert")
             file_path = f"{tmp_dir}/{pack_name}_{rnd_postfix}.tgs"
         else:
-            sticker.file_bytes = img_convertor.convert_to_tt_format(sticker.file_bytes)
+            sticker.file_bytes = img_convertor.convert_to_tt_format(
+                sticker.file_bytes)
             file_path = f"{tmp_dir}/{pack_name}_{rnd_postfix}.png"
 
         result.append(file_path)
--- src/tg_to_tt_stickers/tt_bot.py
+++ src/tg_to_tt_stickers/tt_bot.py
@@ -1,15 +1,22 @@
 import logging
+import os
 from dataclasses import dataclass
 from time import sleep
-import os
 from typing import List, TypedDict
 
 import requests
 from aiohttp import web
 
-from .text_messages import MSG_ERROR, MSG_SET_IN_PROGRESS, MSG_SET_NOT_FOUND, \
-                           MSG_SUCCESS, MSG_WELCOME, MSG_MANY_STICKERS
+from .text_messages import (
+    MSG_ERROR,
+    MSG_MANY_STICKERS,
+    MSG_SET_IN_PROGRESS,
+    MSG_SET_NOT_FOUND,
+    MSG_SUCCESS,
+    MSG_WELCOME,
+)
 from .tg_sticker_downloader import TGStickerDownloader
+
 
 class _MessageDictBase(TypedDict):
     text: str
@@ -25,20 +32,25 @@ class UploadResult:
     file_id: int
     token: str
 
+
 @dataclass
 class User:
     user_id: int
     name: str
     username: str
 
+
 @dataclass
 class MessageBody:
     text: str
+
 
 @dataclass
 class Message:
     sender: User
     body: MessageBody
+
+
 # user add the bot first time
 @dataclass
 class UpdateBotStarted:
@@ -51,7 +63,8 @@ class UpdateBotStarted:
 class UpdateMessageCreated:
     message: Message
 
-class TamTamBot():
+
+class TamTamBot:
 
     routes = web.RouteTableDef()
 
@@ -61,12 +74,12 @@ class TamTamBot():
         self.tg_client = TGStickerDownloader(telegram_bot_token)
         self.log = logging.getLogger()
 
-
-    def api_request(self, method: str, params: dict=None):
+    def api_request(self, method: str, params: dict = None):
         if params is None:
             params = {}
-        params['access_token'] = self.token
-        res = requests.get(f"{self.base_url}/{method}", params=params)  # type: requests.Response
+        params["access_token"] = self.token
+        res = requests.get(f"{self.base_url}/{method}",
+                           params=params)  # type: requests.Response
         if res.status_code == 200:
             return res.text
         else:
@@ -77,66 +90,73 @@ class TamTamBot():
 
     def proceed_bot_started(self, data):
         update = UpdateBotStarted(
-            data['chat_id'],
+            data["chat_id"],
             User(
-                data['user']['user_id'],
-                data['user'].get('name', ""),
-                data['user'].get('username', ""),
-            )
+                data["user"]["user_id"],
+                data["user"].get("name", ""),
+                data["user"].get("username", ""),
+            ),
         )
         self.send_message(update.user.user_id, MSG_WELCOME)
         return web.Response()
 
     def proceed_message_created(self, data):
-        if 'sender' not in data['message']:
+        if "sender" not in data["message"]:
             self.log.error("got bad update: %s", data)
             return web.Response()
         update = UpdateMessageCreated(
             Message(
                 User(
-                    data['message']['sender']['user_id'],
-                    data['message']['sender']['name'],
-                    data['message']['sender'].get('username', "")
+                    data["message"]["sender"]["user_id"],
+                    data["message"]["sender"]["name"],
+                    data["message"]["sender"].get("username", ""),
                 ),
-                MessageBody(data['message']['body'].get("text", ""))
-            )
-        )
+                MessageBody(data["message"]["body"].get("text", "")),
+            ))
 
         tg_set_name = update.message.body.text
 
         tg_set = self.tg_client.get_sticker_pack_by_name(tg_set_name)
         if tg_set is None:
-            self.send_message(update.message.sender.user_id,
-                              MSG_SET_NOT_FOUND.format(tg_set_name=tg_set_name), use_markdown=True)
+            self.send_message(
+                update.message.sender.user_id,
+                MSG_SET_NOT_FOUND.format(tg_set_name=tg_set_name),
+                use_markdown=True,
+            )
             return web.Response()
 
-        self.send_message(update.message.sender.user_id, MSG_SET_IN_PROGRESS.format(tg_set_name=tg_set_name))
+        self.send_message(
+            update.message.sender.user_id,
+            MSG_SET_IN_PROGRESS.format(tg_set_name=tg_set_name),
+        )
         try:
             zip_names = self.tg_client.create_tamtam_zip(tg_set)
         #  TODO: better except
         except Exception as err:
-            self.log.error("error while proceed pack %s: %s",tg_set, err)
+            self.log.error("error while proceed pack %s: %s", tg_set, err)
             self.send_message(update.message.sender.user_id, MSG_ERROR)
             return web.Response()
 
         #  send zip to user
         zip_files = self.upload_files(zip_names)
-        attachments = []
-        for zip_file in zip_files:
-            attachments.append({
-                "type": "file",
-                "payload": {
-                    "token": zip_file.token
-                }
-            })
+        attachments = [{
+            "type": "file",
+            "payload": {
+                "token": zip_file.token
+            }
+        } for zip_file in zip_files]
 
         if len(attachments) == 1:
-            self.send_message(update.message.sender.user_id, MSG_SUCCESS, attachments=attachments)
+            self.send_message(update.message.sender.user_id,
+                              MSG_SUCCESS,
+                              attachments=attachments)
         else:
             self.send_message(update.message.sender.user_id, MSG_SUCCESS)
             self.send_message(update.message.sender.user_id, MSG_MANY_STICKERS)
             for attach in attachments:
-                self.send_message(update.message.sender.user_id, "", attachments=[attach])
+                self.send_message(update.message.sender.user_id,
+                                  "",
+                                  attachments=[attach])
 
         return web.Response()
 
@@ -144,62 +164,67 @@ class TamTamBot():
         data = await request.json()
         self.log.debug("got update from tamtam api: %s", data)
 
-        if 'update_type' not in data:
+        if "update_type" not in data:
             self.log.error("bad update without type: %s", data)
             return web.Response()
 
-        if data['update_type'] == 'bot_started':
+        if data["update_type"] == "bot_started":
             self.proceed_bot_started(data)
 
-        elif data['update_type'] == 'message_created':
+        elif data["update_type"] == "message_created":
             self.proceed_message_created(data)
 
         self.log.error("got unknown update type: %s", data)
         return web.Response()
 
-
     def get_upload_url(self, file_type="file"):
-        params = {
-            "access_token": self.token,
-            "type": file_type
-        }
+        params = {"access_token": self.token, "type": file_type}
         # TODO exceptions
         res = requests.post(f"{self.base_url}/uploads", params=params).json()
         self.log.debug("upload url: %s", res)
-        return res['url']
+        return res["url"]
 
     def upload_files(self, file_paths: List[str]) -> List[UploadResult]:
         upload_results = []
         for file_path in file_paths:
             upload_url = self.get_upload_url()
-            with open(file_path,'rb') as fb:
-                file_to_upload = {'file': (f'{file_path.split("/")[-1]}', fb, 'multipart/form-data')}
+            with open(file_path, "rb") as fb:
+                file_to_upload = {
+                    "file":
+                    (f'{file_path.split("/")[-1]}', fb, "multipart/form-data")
+                }
                 res = requests.post(upload_url, files=file_to_upload).json()
                 os.remove(file_path)
-                upload_results.append(UploadResult(res['fileId'], res['token']))
+                upload_results.append(UploadResult(res["fileId"],
+                                                   res["token"]))
         return upload_results
 
-
-    def send_message(self, user_id: int, text: str, attachments: list=None,
-                     no_link_preview=True, use_markdown=False):
+    def send_message(
+        self,
+        user_id: int,
+        text: str,
+        attachments: list = None,
+        no_link_preview=True,
+        use_markdown=False,
+    ):
         param = {
             "access_token": self.token,
             "user_id": user_id,
-            "disable_link_preview": no_link_preview
+            "disable_link_preview": no_link_preview,
         }
-        data :MessageDict = {
-            "text": text
-        }
+        data: MessageDict = {"text": text}
         if attachments is not None:
-            data['attachments'] = attachments
+            data["attachments"] = attachments
         if use_markdown:
-            data['format'] = 'markdown'
+            data["format"] = "markdown"
 
         not_ok = True
         max_tries = 5
         sleep_time = 1
         while not_ok:
-            res = requests.post(f"{self.base_url}/messages", json=data, params=param)
+            res = requests.post(f"{self.base_url}/messages",
+                                json=data,
+                                params=param)
             self.log.info("sending msg to user %s: %s", user_id, text)
             if res.status_code == 200:
                 not_ok = False
@@ -207,21 +232,30 @@ class TamTamBot():
             # It may take time for the server to process your file (audio/video or any binary).
             # While a file is not processed you can't attach it. It means the last step will fail with 400 error.
             # Try to send a message again until you'll get a successful result.
-            if res.status_code == 400 and "file.not.processed" in res.json()["message"]:
+            if res.status_code == 400 and "file.not.processed" in res.json(
+            )["message"]:
                 self.log.debug("files not ready, sleep and retry...")
                 sleep(sleep_time)
                 max_tries -= 1
                 sleep_time += 1
                 if max_tries == 0:
-                    self.send_message(user_id, "Не получилось загрузить файл в ТамТам, попробуй еще раз")
+                    self.send_message(
+                        user_id,
+                        "Не получилось загрузить файл в ТамТам, попробуй еще раз",
+                    )
                     return web.Response()
                 continue
 
             if res.status_code != 200:
-                self.log.error("can't send msg to user %s, statis: %s %s", user_id, res.status_code, res.text)
+                self.log.error(
+                    "can't send msg to user %s, statis: %s %s",
+                    user_id,
+                    res.status_code,
+                    res.text,
+                )
                 return web.Response()
 
     def run(self):
         app = web.Application()
-        app.add_routes([web.post('/', self.proceed)])
+        app.add_routes([web.post("/", self.proceed)])
         web.run_app(app, port=19999)
